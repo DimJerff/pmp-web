b12aa491c3a072899c66dac7e4d08a4aedddc024
gaojie <gaojie@limei.com>
2015-08-13 14:07:41 +0800

    sql流程测试2：上线成功，数据成功，执行git push

--- migrations/1.2.1/update.1.sql
+++ migrations/1.2.1/update.1.sql
@@ -6,7 +6,7 @@ use cheetahx;
 -- 关闭自动提交，开启事务
 SET AUTOCOMMIT = 0;
 
-INSERT INTO `c_base_operation_object` (`id`, `key`, `name`) VALUES ('5', 'test', '测试sql上线');
+INSERT INTO `c_base_operation_object` (`id`, `key`, `name`) VALUES ('6', 'test', '测试sql上线');
 
 -- 提交所有sql，如果失败，则全部失败
 commit;

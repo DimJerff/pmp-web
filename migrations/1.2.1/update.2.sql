736cd69ad9b840858c06b25eccb7d630564ca1d1
gaojie <gaojie@limei.com>
2015-08-13 14:16:26 +0800

    sql流程测试3：第二条sql，上线成功，数据成功，执行git push

---
+++ migrations/1.2.1/update.2.sql
@@ -0,0 +1,12 @@
+-- 设定sql的字符编码
+SET character_set_client = utf8;
+SET character_set_connection = utf8;
+use cheetahx;
+
+-- 关闭自动提交，开启事务
+SET AUTOCOMMIT = 0;
+
+INSERT INTO `c_base_operation_object` (`id`, `key`, `name`) VALUES ('7', 'test', '测试sql上线2');
+
+-- 提交所有sql，如果失败，则全部失败
+commit;
